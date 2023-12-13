2d2ea3da4fc7c8eecd5e0ffe52010d3b09b4aa30
Linsiji <42350425+linsijiLaGuara@users.noreply.github.com>
2023-12-12 21:42:45 -0300

    lista terminada

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+//const addboton = document.getElementById("addboton").innerHTML
+
+lista =[]
+
+//crear un fucion que agregue elementos a la lista atraves de input haciendi clic en el boton
+
+function addtask(){
+
+  const addinput = document.getElementById("full-wid-input").value
+
+  lista.push(addinput)
+  console.log(lista)
+// renderizar donde se muentran los item de la lista
+
+
+}
