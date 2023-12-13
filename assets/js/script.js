33b4a4513e2547280eb3c5385b52804e2701ce28
Linsiji <42350425+linsijiLaGuara@users.noreply.github.com>
2023-12-12 23:00:24 -0300

    lista mostrada

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,59 @@
 //const addboton = document.getElementById("addboton").innerHTML
 
-lista =[]
-
+lista = [
+  {
+    'id':1,
+    'nombreTarea':"ejemplo 1",
+    'tareaTerminada': false,
+    'tareaEliminada': false
+  },
+  {
+    'id':2,
+    'nombreTarea':"ejemplo 2",
+    'tareaTerminada': false,
+    'tareaEliminada': false
+  },
+  {
+    'id':3,
+    'nombreTarea':"ejemplo 3",
+    'tareaTerminada': false,
+    'tareaEliminada': false
+  },
+];
+contador = 1;
 //crear un fucion que agregue elementos a la lista atraves de input haciendi clic en el boton
 
-function addtask(){
+function addtask() {
+  const addinput = document.getElementById("full-wid-input").value;
+  let nuevaTarea = {
+    id: contador,
+    nombreTarea: addinput,
+    tareaTerminada: false,
+    tareaEliminada: false,
+  };
 
-  const addinput = document.getElementById("full-wid-input").value
-
-  lista.push(addinput)
-  console.log(lista)
-// renderizar donde se muentran los item de la lista
-
-
+  lista.push(nuevaTarea);
+  console.log(lista);
+  contador++;
+  mostar();
+  // renderizar donde se muentran los item de la lista
 }
+
+function renderizarListaHtml(tarea) {
+  let inicial = "<ul > ";
+  let id = "<li> " + tarea.id + "</li>";
+  let tareas = "<li> " + tarea.nombreTarea + "</li>";
+  let final = "</ul>";
+
+  return inicial + id + tareas + final;
+}
+function mostar() {
+  let datos = "";
+
+  for (const tarea of lista) {
+    datos += renderizarListaHtml(tarea);
+  }
+  document.getElementById("boxList").innerHTML = datos;
+  document.getElementById("total-tareas").innerHTML = lista.length;
+}
+mostar()
